1fe4aa899cc797d305ef3b8dc16eae1c9c654b41
EthanCote5233 <ECote523@Gmail.com>
2025-07-30 21:31:10 -0400

    Security fixes

--- Frontier/Endpoints/Account/DeleteAccountcs.cs
+++ Frontier/Endpoints/Account/DeleteAccountcs.cs
@@ -1,21 +1,23 @@
 ﻿using FastEndpoints;
-using Frontier.Contracts.Requests;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Frontier.Endpoints.Account
 {
-    public class DeleteAccount(IAccountOperations accounts) : Endpoint<UserIdRequest, AccountShard>
+    public class DeleteAccount(IAccountOperations accounts) : EndpointWithoutRequest
     {
         public override void Configure()
         {
             Delete("/account");
         }
 
-        public override async Task HandleAsync(UserIdRequest request, CancellationToken cancellationToken)
+        public override async Task HandleAsync(CancellationToken cancellationToken)
         {
-            await accounts.DeleteUserAsync(request.UserId);
-            await Send.NoContentAsync(cancellationToken); ;
+            long userId = long.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+            await accounts.DeleteUserAsync(userId);
+            await Send.NoContentAsync(cancellationToken);
         }
     }
 }
---
+++ Frontier/Endpoints/Circle/GetUserCircles.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using Frontier.Contracts.Requests;
+using Microsoft.AspNetCore.Identity;
+using Repository.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frontier.Endpoints.Circle
+{
+    public class GetUserCircles(IAccountOperations accounts) : Endpoint<UserIdRequest, List<CircleShard>>
+    {
+        public override void Configure()
+        {
+            Get("/circle");
+        }
+
+        public override async Task HandleAsync(UserIdRequest request, CancellationToken cancellationToken)
+        {
+            CoreUser user = await userManager.GetUserAsync(HttpContext.User);
+
+            return Ok(await circles.GetUserCirclesAsync(user.Id));
+        }
+    }
+}

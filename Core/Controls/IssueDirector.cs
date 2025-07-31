6e092eed2c8758fff46030928ef857cf4fdac031
jackalope@thehollowcorporation.onmicrosoft.com <jackalope@thehollowcorporation.onmicrosoft.com>
2025-07-31 15:49:19 -0400

    Nuke Issue Director

--- Core/Controls/IssueDirector.cs
+++ Core/Controls/IssueDirector.cs
@@ -13,240 +13,21 @@ namespace Core.Controls
 {
     internal class IssueDirector : AbstractDirector, IIssueOperations
 	{
-		#region Initialisation
-
 		public IssueDirector(CoreTerminal terminal) : base(terminal) { }
 
-		#endregion
-
-		#region Operations
-
-        public async Task<PostShard> GetPostAsync(long userId, long snapshotId)
+        public Task<PostShard> AddPostAsync(long userId, long issueId, DateTimeOffset timestamp, string caption, MemoryStream image)
         {
-            var user = await GetUserAsync(userId);
-            var snapshot = await Issues.GetPostAsync(snapshotId);
-
-            var snapshotOwner = await GetUserAsync(snapshot.User.Id);
-            var gathering = await GetGatheringAsync(snapshot.GatheringId);
-
-            // Fail if user is blocked
-            FailIf(await user.IsBlockedBy(snapshotOwner),
-                new UserErrorException(UserErrorCode.CANNOT_VIEW));
-
-            // Fail if user cannot view gathering
-            Verify(await user.CanView(gathering),
-                new UserErrorException(CircleErrorCode.CANNOT_VIEW));
-
-            return snapshot;
+            throw new NotImplementedException();
         }
 
-        public async Task<GalleryShard> GetPostsForIssueAsync(long userId, long gatheringId)
+        public Task DeletePostAsync(long userId, long postId)
         {
-            var user = await GetUserAsync(userId);
-            var gathering = await GetCircleAsync(gatheringId);
-
-            // Fail if user cannot view gathering
-            Verify(await user.CanView(gathering),
-                new UserErrorException(CircleErrorCode.CANNOT_VIEW));
-
-            GalleryShard gallery = new(new());
-
-            if (await gathering.HasOnGuestList(user))
-            {
-                // Remove any snapshots from blocked or blocking users
-                GalleryShard filteredGallery = new(await RemoveBlockedSnapshotsAsync(user, await gathering.Posts));
-
-                // Remove strangers if gallery is in pre mode
-                if (gathering.IsUpcoming)
-                {
-                    var strangers = await Profiles.ReturnStrangerDangerAsync(user.Id, filteredGallery.Snapshots.Select(snapshot => snapshot.User.Id).ToArray());
-
-                    strangers.Remove(gathering.HostId);
-                    strangers.Remove(user.Id);
-
-                    gallery = new(HideStrangersAsync(filteredGallery.Snapshots, strangers));
-                }
-            }
-            // Check if any companions attended
-            else
-            {
-                var companionIds = (await user.Companions)
-                    .ConvertAll(companion => companion.Id);
-
-                var companionSnapshots = (await gathering.Posts)
-                    .Where(snapshot => companionIds.Contains(snapshot.User.Id)).ToList();
-
-                gallery = new(companionSnapshots);
-            }
-
-            return gallery;
+            throw new NotImplementedException();
         }
 
-
-        public async Task<PostShard> AddPostAsync(long userId, long gatheringId, MemoryStream image)
+        public Task EditPostAsync(long userId, long postId, DateTimeOffset? timestamp = null, string caption = null, MemoryStream image = null)
         {
-            var userSync = GetUserAsync(userId);
-            var targetGatheringSync = GetCircleAsync(gatheringId);
-            var user = await userSync;
-            var gathering = await targetGatheringSync;
-
-            await user.CanPostTo(gathering);
-
-            // Try to etch
-            var snapshot = await Issues.AddPostAsync(gathering.Id, user.Id, Time);
-
-            try
-            {
-                // Save image
-                await Terminal.MediaDirector.UploadSnapshotAsync(user.Id, snapshot.Id, image);
-            }
-            catch (Exception ex)
-            {
-                // If failed, remove snapshot
-                await Issues.HardDeleteAsync(snapshot.Id);
-                throw new UnexpectedFailureException($"Failed to upload snapshot for user {userId} at {gatheringId}.", ex, HollowErrorCode.UPLOAD_FAILED);
-            }
-
-            return snapshot;
-        }
-
-        public async Task DeletePostAsync(long userId, long snapshotId)
-        {
-            var userSync = GetUserAsync(userId);
-            var snapshot = await Issues.GetPostAsync(snapshotId);
-            var gatheringTaken = await GetGatheringAsync(snapshot.GatheringId);
-            var user = await userSync;
-
-            // Verify user owns the snapshot or can modify the gathering
-            Verify(user.Owns(snapshot) || gatheringTaken.IsModifiableBy(user),
-                new UserErrorException(IssueErrorCode.CANNOT_DELETE));
-
-            await Issues.SoftDeleteAsync(snapshot.Id);
-        }
-
-        public async Task AcclaimSnapshotAsync(long userId, long snapshotId, SnapshotAcclaim acclaim)
-        {
-            var userSync = GetUserAsync(userId);
-            var snapshot = await Issues.GetPostAsync(snapshotId);
-            var gatheringTaken = await GetGatheringAsync(snapshot.GatheringId);
-            var user = await userSync;
-
-            // Verify user can interact with snapshot
-            Verify(await gatheringTaken.WasAttendedBy(user),
-                new UserErrorException(IssueErrorCode.CANNOT_INTERACT));
-
-            FailIf(user.Owns(snapshot),
-                new UserErrorException(IssueErrorCode.CANNOT_INTERACT_SELF));
-
-            // Check action
-            if (acclaim == SnapshotAcclaim.Acclaim)
-            {
-                await Issues.AcclaimSnapshotAsync(snapshot.Id, user.Id);
-            }
-            else
-            {
-                await Issues.DeleteSnapshotAcclaimAsync(snapshot.Id, user.Id);
-            }
-        }
-
-        public async Task<ColumnShard>
-            GetWallAsync(long userId, int depth, int lastDepth)
-        {
-            var user = await GetUserAsync(userId);
-            Dictionary<long, GatheringHeader> gatheringHeaders = new();
-
-            // Enforce lastDepth < depth
-            lastDepth = Math.Min(lastDepth, depth - 1);
-
-            // Retrieve companion-populated gathering snapshots after a specified time excluding previously viewed gatherings
-            DateTimeOffset depthCharge = Time - TimeSpan.FromDays(depth);
-            DateTimeOffset lastDepthCharge = Time - TimeSpan.FromDays(lastDepth);
-            var generatedWall = await Issues.GenerateColumnForUserAsync(user.Id, depthCharge, lastDepthCharge);
-
-            // Get the respective gathering headers for the snapshots
-            foreach (var snapshot in generatedWall)
-            {
-                var gatheringId = snapshot.GatheringId;
-
-                // Add gathering header if it does not yet exist
-                if (!gatheringHeaders.ContainsKey(gatheringId))
-                {
-                    var etchedGathering = await GetGatheringAsync(gatheringId);
-
-                    gatheringHeaders.Add(gatheringId, etchedGathering.ToGatheringHeader(snapshot.TimeTaken));
-                }
-                // Update gathering header active time if snapshot is more recent
-                else if (HappenedBefore(gatheringHeaders[gatheringId].LastActiveTime, snapshot.TimeTaken))
-                {
-                    gatheringHeaders[gatheringId] = gatheringHeaders[gatheringId] with { LastActiveTime = snapshot.TimeTaken };
-                }
-            }
-
-            return new(gatheringHeaders.Values.ToList(), generatedWall);
-        }
-
-        #endregion
-
-        #region Favours
-
-        internal async Task<List<Issue>> RequestCircleIssuesAsync(Circle circle)
-            => (await Issues.GetIssuesForCircleAsync(circle.Id)).ConvertAll(issue => new Issue(issue));
-
-		internal async Task<List<CorePost>> RequestIssuePostsAsync(Issue gathering)
-            => await Issues.GetPostsForIssueAsync(gathering.Id);
-
-		internal async Task<List<PostShard>> RequestVisiblePostsAsync(User user, Issue issue)
-        {
-            // Verify user can see the issue
-            if (!await user.CanView(issue))
-            {
-                return new List<PostShard> { };
-            }
-
-            _ = issue.Posts.Sync();
-
-            // Determine the level of visibility to the user
-
-            // Check if the user attended the gathering
-            if (await issue.WasAttendedBy(user) || issue.IsModifiableBy(user))
-            {
-                return await issue.Posts;
-            }
-            else
-            {
-                // Get all companion snapshots
-                var companions = await user.Companions;
-
-                var companionSnapshots = (await issue.Posts)
-                    .Where(snapshot => companions.Exists(cmp => cmp.Id.Equals(snapshot.User.Id)))
-                    .ToList();
-
-                return companionSnapshots;
-            }
-        }
-
-        internal async Task<List<PostShard>>
-            RemoveBlockedPostsAsync(User user, List<PostShard> posts)
-        {
-            // Probably want a more client friendly way to hide blocked posts
-
-            List<PostShard> accessiblePosts = new();
-
-            foreach (PostShard post in posts)
-            {
-                if (user.Id != post.User.Id)
-                {
-                    User snapshotOwner = await GetUserAsync(post.User.Id);
-
-                    // Check if blocking link exists
-                    if (await user.IsBlocking(snapshotOwner) || await user.IsBlockedBy(snapshotOwner))
-                    { continue; }
-                }
-
-                accessiblePosts.Add(post);
-            }
-
-            return accessiblePosts;
+            throw new NotImplementedException();
         }
 
         public Task<IssueShard> GetIssueAsync(long userId, long issueId)
@@ -259,17 +40,15 @@ namespace Core.Controls
             throw new NotImplementedException();
         }
 
-        public Task<PostShard> AddPostAsync(long userId, long issueId, DateTimeOffset timestamp, string caption, MemoryStream image)
+        public Task<PostShard> GetPostAsync(long userId, long postId)
         {
             throw new NotImplementedException();
         }
 
-        public Task EditPostAsync(long userId, long postId, DateTimeOffset? timestamp = null, string caption = null, MemoryStream image = null)
+        public Task<GalleryShard> GetPostsForIssueAsync(long userId, long issueId)
         {
             throw new NotImplementedException();
         }
-
-        #endregion
     }
 }
 

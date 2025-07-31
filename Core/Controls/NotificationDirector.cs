787f4dcb36e0b8dda339253786892f4911cbda08
jackalope@thehollowcorporation.onmicrosoft.com <jackalope@thehollowcorporation.onmicrosoft.com>
2025-07-31 15:53:51 -0400

    Convert Notification Director

--- Core/Controls/NotificationDirector.cs
+++ Core/Controls/NotificationDirector.cs
@@ -1,72 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Core.Boundaries;
 using Core.Entities;
 using Core.Notifications;
-
-using static Core.Entities.Psijic;
 using static Core.Entities.Artificer;
 
 namespace Core.Controls
 {
     internal class NotificationDirector : AbstractDirector, INotificationOperations
 	{
-		#region Initialisation
-
 		public NotificationDirector(CoreTerminal terminal) : base(terminal) { }
-
-		#endregion
-
-		#region Operations
 
 		public async Task<NotificationPreferencesShard> GetNotificationPreferencesAsync(long userId)
 		{
 			var user = await GetUserAsync(userId);
 			var profile = await user.NotificationProfile;
 
-			return new(profile.NotificationId, profile.SocialInvitations, profile.CompanionActivity,
-				profile.GatheringReminders, profile.GatheringActivity, profile.GatheringDiscovery);
+			return new(profile.NotificationId, profile.IssuePosts, profile.IssueReminders);
 		}
 
-        public async Task UpdateNotificationPreferencesAsync(long userId,
-            bool? socialInvitations = null, bool? companionActivity = null,
-			bool? gatheringReminders = null, bool? gatheringActivity = null,
-			bool? gatheringDiscovery = null)
-		{
-			var user = await GetUserAsync(userId);
+        public async Task UpdateNotificationPreferencesAsync(long userId, bool? issuePosts = null, bool? issueReminders = null)
+        {
+            var user = await GetUserAsync(userId);
 
             List<(string Property, object Value)> edits = new();
 
-            if (IsNotNull(socialInvitations))
-			{
-                edits.Add((nameof(NotificationProfile.SocialInvitations), socialInvitations.Value));
+            if (IsNotNull(issuePosts))
+            {
+                edits.Add((nameof(NotificationProfile.IssuePosts), issuePosts.Value));
             }
-            if (IsNotNull(companionActivity))
-			{
-                edits.Add((nameof(NotificationProfile.CompanionActivity), companionActivity.Value));
-            }
-            if (IsNotNull(gatheringReminders))
-			{
-                edits.Add((nameof(NotificationProfile.GatheringReminders), gatheringReminders.Value));
-            }
-            if (IsNotNull(gatheringActivity))
-			{
-                edits.Add((nameof(NotificationProfile.GatheringActivity), gatheringActivity.Value));
-            }
-            if (IsNotNull(gatheringDiscovery))
-			{
-                edits.Add((nameof(NotificationProfile.GatheringDiscovery), gatheringDiscovery.Value));
+            if (IsNotNull(issueReminders))
+            {
+                edits.Add((nameof(NotificationProfile.IssueReminders), issueReminders.Value));
             }
 
-			await Notifications.UpdateNotificationProfileAsync(user.Id, edits);
-		}
-
-		#endregion
-
-		#region Favours
+            await Notifications.UpdateNotificationProfileAsync(user.Id, edits);
+        }
 
 		internal async Task<NotificationProfile> RequestNotificationProfileAsync(User user)
 		{
@@ -114,7 +85,5 @@ namespace Core.Controls
 				await Terminal.NotificationService.CancelNotification(id);
 			}
 		}
-
-		#endregion
-	}
+    }
 }

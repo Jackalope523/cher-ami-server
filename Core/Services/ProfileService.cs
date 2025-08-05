ed9b0b10ecfe31acfd16bb6ca8ff56a9b37595a0
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-05 08:47:17 -0400

    Add DI to Profile Service

--- Core/Services/ProfileService.cs
+++ Core/Services/ProfileService.cs
@@ -1,38 +1,31 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Core.Boundaries;
 using Core.Entities;
-using Core.Notifications;
-using Microsoft.Extensions.Logging;
-
-using static Core.Entities.Arbiter;
 
 namespace Core.Services
 {
-    public class ProfileService : AbstractService, IProfileService
+    public class ProfileService(IProfileRepository profileRepository) : IProfileService
 	{
-		public ProfileService(CoreTerminal terminal) : base(terminal) { }
-        public Task BlockUserAsync(long userId, long targetId)
+        public async Task BlockUserAsync(long userId, long targetId)
+        {
+            await profileRepository.BlockUserAsync(userId, targetId, Psijic.Time);
+        }
+
+        public async Task<List<BlockedUserShard>> GetBlockedUsersAsync(long userId)
+        {
+            return await profileRepository.GetBlockedUsersAsync(userId);
+        }
+
+        public async Task<ProfileShard> GetProfileAsync(long userId, long targetId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<BlockedUserShard>> GetBlockedUsersAsync(long userId)
+        public async Task UnblockUserAsync(long userId, long targetId)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<ProfileShard> GetProfileAsync(long userId, long targetId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UnblockUserAsync(long userId, long targetId)
-        {
-            throw new NotImplementedException();
+            await profileRepository.UnblockUserAsync(userId, targetId);
         }
     }
 }

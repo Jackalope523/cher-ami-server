8362c552a933e9274fac114af754dcba62a14621
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-08 15:45:12 -0400

    Move Update Notification Preferences to fast endpoint

---
+++ Frontier/Endpoints/Notifications/UpdateNotificationPreferencesEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Notifications
+{
+    public class UpdateNotificationPreferencesRequest
+    {
+        public bool? IssuePosts { get; set; }
+        public bool? IssueReminders { get; set; }
+    }
+
+    public class UpdateNotificationPreferencesRequestValidator : Validator<UpdateNotificationPreferencesRequest>
+    {
+        public UpdateNotificationPreferencesRequestValidator()
+        {
+            RuleFor(x => x)
+                .Must(x => x.IssuePosts.HasValue || x.IssueReminders.HasValue)
+                .WithMessage("At least one of IssuePosts or IssueReminders must be provided.");
+        }
+    }
+
+
+    public class UpdateNotificationPreferencesEndpoint(INotificationStorageService notificationService) : Endpoint<UpdateNotificationPreferencesRequest>
+    {
+        public override void Configure()
+        {
+            Post("/notifications");
+        }
+
+        public override async Task HandleAsync(UpdateNotificationPreferencesRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            await notificationService.UpdateNotificationPreferencesAsync(userId, request.IssuePosts, request.IssueReminders);
+            await Send.NoContentAsync(cancellationToken);
+        }
+    }
+}

ae578326d57d59c6a111634e1cb74b33b288ce1f
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-10 15:04:30 -0400

    Move Delete Post to fast endpoint

---
+++ Frontier/Endpoints/Issues/DeletePostEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Shared.Requests;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frontier.Endpoints.Account
+{
+    public class DeletePostEndpoint(IIssueService issues) : Endpoint<PostIdRequest>
+    {
+        public override void Configure()
+        {
+            Delete("/issues/posts/{postId}");
+        }
+
+        public override async Task HandleAsync(PostIdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            await issues.DeletePostAsync(userId, request.Id);
+            await Send.NoContentAsync(cancellationToken);
+        }
+    }
+}

4e32675e88fdab46bf1b7ce7c817efbf4a07207f
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-10 15:06:07 -0400

    Move Get Post to fast endpoint

---
+++ Frontier/Endpoints/Issues/GetPostEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Contracts.Responses;
+using LazyLizardBackend.Shared.Mappers;
+using LazyLizardBackend.Shared.Requests;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Issue
+{
+    public class GetPostEndpoint(IIssueService issues) : Endpoint<PostIdRequest, List<PostDTO>, PostResponseMapper>
+    {
+        public override void Configure()
+        {
+            Get("/issues/posts/{postId}");
+        }
+
+        public override async Task HandleAsync(PostIdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            CorePost corePost = await issues.GetPostAsync(userId, request.Id);
+            await SendMapped(corePost, 200, cancellationToken);
+        }
+    }
+}

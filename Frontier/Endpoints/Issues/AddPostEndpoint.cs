05bfc478b66c7e97b5df210fe73001d8f4e346b4
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-10 15:04:20 -0400

    Move Add Post to fast endpoint

---
+++ Frontier/Endpoints/Issues/AddPostEndpoint.cs
@@ -0,0 +1,68 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Shared.Mappers;
+using LazyLizardBackend.Shared.Requests;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using System.IO;
+using LazyLizardBackend.Contracts.Responses;
+using LazyLizardBackend.Endpoints.Issue;
+
+namespace Frontier.Endpoints.Account
+{
+    public class CreatePostRequest
+    {
+        public long IssueId { get; set; }
+        public DateTime Time { get; set; }
+        public string Caption { get; set; }
+        public IFormFile Image { get; set; }
+    }
+
+    public class CreatePostRequestValidator : Validator<CreatePostRequest>
+    {
+        public CreatePostRequestValidator()
+        {
+            RuleFor(x => x.IssueId)
+               .GreaterThan(0).WithMessage("IssueId must be greater than 0.");
+
+            RuleFor(x => x.Time)
+                .NotEmpty().WithMessage("Time is required.");
+
+            RuleFor(x => x.Image)
+                .NotNull().WithMessage("Image is required.")
+                .Must(file => file.Length > 0).WithMessage("Uploaded image can not be empty.");
+
+            RuleFor(x => x.Caption)
+                .MaximumLength(200).WithMessage("Caption cannot exceed 200 characters.");
+        }
+    }
+
+    public class AddPostEndpoint(IIssueService issues) : Endpoint<CreatePostRequest, PostDTO, PostResponseMapper>
+    {
+        public override void Configure()
+        {
+            Post("/issues/{issueId}/posts");
+            AllowFileUploads();
+        }
+
+        public override async Task HandleAsync(CreatePostRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            using var stream = new MemoryStream();
+            await request.Image.CopyToAsync(stream, cancellationToken);
+
+            CorePost corePost = await issues.AddPostAsync(userId, request.IssueId, request.Time, request.Caption, stream);
+
+            await Send.CreatedAtAsync<GetPostEndpoint>
+                (
+                    new PostIdRequest() { Id = corePost.Id }, 
+                    Map.FromEntity(corePost), 
+                    cancellation: cancellationToken
+                );
+        }
+    }
+}

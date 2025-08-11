459e5a1c6931966bb96822934d7fc3e7763b7416
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-11 15:49:57 -0400

    Move Get Avatar Metadata to fast endpoint

---
+++ Frontier/Endpoints/Media/GetAvatarMetadataEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Contracts.Requests;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Media
+{
+    public class GetAvatarMetadataEndpoint(IMediaService mediaService) : Endpoint<IdRequest, ImageMetadataDTO>
+    {
+        public override void Configure()
+        {
+            Get("/media/avatars/{id}/metadata");
+        }
+
+        public override async Task HandleAsync(IdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            ImageMetadataDTO response = await mediaService.GetAvatarMetadataAsync(userId, request.Id);
+            await Send.OkAsync(response, cancellationToken);
+        }
+    }
+}

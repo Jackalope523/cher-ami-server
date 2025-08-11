08766603854b46f964cd19f6c6a1bb8f74968c7e
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-11 15:50:15 -0400

    Move Get Header Metadata to fast endpoint

---
+++ Frontier/Endpoints/Media/GetHeaderMetadataEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Contracts.Requests;
+using LazyLizardBackend.Shared.Responses;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Media
+{
+    public class GetHeaderMetadataEndpoint(IMediaService mediaService) : Endpoint<IdRequest, ImageMetadataDTO>
+    {
+        public override void Configure()
+        {
+            Get("/media/headers/{id}/metadata");
+        }
+
+        public override async Task HandleAsync(IdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            ImageMetadataDTO response = await mediaService.GetHeaderMetadataAsync(userId, request.Id);
+            await Send.OkAsync(response, cancellationToken);
+        }
+    }
+}

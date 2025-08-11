f1cb1feb76e9b445fa169e7ea3c805b6e3f87978
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-11 15:50:04 -0400

    Move Get Header to fast endpoint

---
+++ Frontier/Endpoints/Media/GetHeaderEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Contracts.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Media
+{
+    public class GetHeaderEndpoint(IMediaService mediaService) : Endpoint<IdRequest, FileStreamResult>
+    {
+        public override void Configure()
+        {
+            Get("/media/headers/{id}");
+        }
+
+        public override async Task HandleAsync(IdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            MemoryStream imageStream = await mediaService.GetHeaderAsync(userId, request.Id);
+
+            if (imageStream != null)
+            {
+                imageStream.Seek(0, SeekOrigin.Begin);
+
+                FileStreamResult response = new(imageStream, "image/jpeg")
+                {
+                    FileDownloadName = "header.jpg"
+                };
+
+                await Send.OkAsync(response, cancellationToken);
+            }
+            else
+            {
+                await Send.NotFoundAsync(cancellationToken);
+            }
+        }
+    }
+}

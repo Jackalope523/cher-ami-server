7530be6558ce786fb9be291dd440d8f96a003231
Jackalope523 <jackalope@thehollowcorporation.com>
2025-08-11 15:50:38 -0400

    Move Get Post Metadata to fast endpoint

---
+++ Frontier/Endpoints/Media/GetPostMetadataEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using LazyLizardBackend.Contracts.Requests;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LazyLizardBackend.Endpoints.Media
+{
+    public class GetPostMetadataEndpoint(IMediaService mediaService) : Endpoint<IdRequest, ImageMetadataDTO>
+    {
+        public override void Configure()
+        {
+            Get("/media/posts/{id}/metadata");
+        }
+
+        public override async Task HandleAsync(IdRequest request, CancellationToken cancellationToken)
+        {
+            long userId = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            ImageMetadataDTO response = await mediaService.GetPostMetadataAsync(userId, request.Id);
+            await Send.OkAsync(response, cancellationToken);
+        }
+    }
+}

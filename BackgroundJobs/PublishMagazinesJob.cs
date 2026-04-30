5ed739e42dbabd63fee8269754a1710449f99c17
EthanCote5233 <ECote523@Gmail.com>
2026-04-30 19:45:57 -0400

    Hook up new layouts into the magazine publishing job

--- BackgroundJobs/PublishMagazinesJob.cs
+++ BackgroundJobs/PublishMagazinesJob.cs
@@ -2,12 +2,15 @@
 using Azure.Identity;
 using Azure.Storage.Blobs;
 using CherAmiAPI.Components;
+using CherAmiAPI.Components.Layouts;
+using CherAmiAPI.Components.Pages;
 using CherAmiAPI.Contexts;
 using CherAmiAPI.Entities;
 using CherAmiAPI.Interfaces;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
 using Quartz;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
@@ -60,16 +63,23 @@ namespace CherAmiAPI.BackgroundJobs
 
 
 
-            Log.Error("Getting issues...");
+            //Log.Error("Getting issues...");
             //var issues = await ctx.Issues
             //                      .Where(x => x.Status == IssueStatus.Drafting && x.DraftingEnd < DateTimeOffset.UtcNow)
             //                      .Select(x => new { Issue = x, CircleId = x.Circle.Id, CircleTitle = x.Circle.Title })
             //                      .ToListAsync();
 
+            Log.Error("Getting issues...");
             var issues = await ctx.Issues
-                             .Where(x => x.CircleId == 68 || x.CircleId == 71)
-                             .Select(x => new { Issue = x, CircleId = x.Circle.Id, CircleTitle = x.Circle.Title })
-                             .ToListAsync();
+                                  .Where(x => x.Status == IssueStatus.Drafting)
+                                  .Select(x => new { Issue = x, CircleId = x.Circle.Id, CircleTitle = x.Circle.Title })
+                                  .ToListAsync();
+
+            //Log.Error("Getting issues...");
+            //var issues = await ctx.Issues
+            //                      .Where(x => x.Id == 180)
+            //                      .Select(x => new { Issue = x, CircleId = x.Circle.Id, CircleTitle = x.Circle.Title })
+            //                      .ToListAsync();
 
             foreach (var issue in issues)
             {
@@ -81,13 +91,13 @@ namespace CherAmiAPI.BackgroundJobs
 
                 Log.Error($"Getting posts from database...");
                 var posts = await ctx.Posts
-                                  .Where(x => x.IssueId == issue.Issue.Id)
-                                  .Select(x => new { x.LowResolutionImagePath, x.Author.AvatarPath, AuthorName = $"{x.Author.FirstName} {x.Author.LastName}", Text = x.Caption })
-                                  .ToListAsync();
+                                      .Where(x => x.IssueId == issue.Issue.Id)
+                                      .Select(x => new { x.LowResolutionImagePath, x.ImageWidth, x.ImageHeight, x.Author.AvatarPath, AuthorName = $"{x.Author.FirstName} {x.Author.LastName}", Text = x.Caption })
+                                      .ToListAsync();
 
                 if (recipients.Count == 0 || posts.Count == 0)
                 {
-                    Log.Error($"Marking issue {issue.Issue.Id} as unreleased.");
+                    //Log.Error($"Marking issue {issue.Issue.Id} as unreleased.");
                     //issue.Issue.Status = posts.Count == 0 ? IssueStatus.Empty : IssueStatus.Unreleased;
 
                     //Issue toAddAnyway = new()
@@ -113,6 +123,8 @@ namespace CherAmiAPI.BackgroundJobs
                     PostComponentProps props = new()
                     {
                         Image = await DownloadBlobAsync(post.LowResolutionImagePath),
+                        ImageHeight = post.ImageHeight,
+                        ImageWidth = post.ImageWidth,
                         AuthorAvatar = post.AvatarPath != null ? await DownloadBlobAsync(post.AvatarPath) : null,
                         AuthorName = post.AuthorName,
                         Text = post.Text
@@ -120,6 +132,8 @@ namespace CherAmiAPI.BackgroundJobs
 
                     postComponentProps.Add(props);
                 }
+
+                List<(Template template, List<PostComponentProps> posts)> templatePostPairs = MagazinePlanner.Plan(postComponentProps);
 
                 Log.Error($"Making PDFs...");
                 foreach (Recipient recipient in recipients) 
@@ -191,7 +205,7 @@ namespace CherAmiAPI.BackgroundJobs
                                 });
                         });
 
-                        for (int i = 0; i < postComponentProps.Count; i+=4)
+                        foreach (var entry in templatePostPairs)
                         {
                             container.Page(page =>
                             {
@@ -199,48 +213,29 @@ namespace CherAmiAPI.BackgroundJobs
                                 page.Margin(0.5f, Unit.Inch);
                                 page.PageColor(white);
 
+                                Page pageContent = entry.template switch
+                                {
+                                    TemplateA => new PageA(entry.posts),
+                                    TemplateB => new PageB(entry.posts),
+                                    TemplateC => new PageC(entry.posts),
+                                    TemplateD => new PageD(entry.posts),
+                                    TemplateE => new PageE(entry.posts),
+                                    TemplateF => new PageF(entry.posts),
+                                    TemplateG => new PageG(entry.posts),
+                                    TemplateH => new PageH(entry.posts),
+                                    TemplateI => new PageI(entry.posts),
+                                    TemplateJ => new PageJ(entry.posts),
+                                    _ => throw new InvalidOperationException($"Unknown template: {entry.template.GetType().Name}")
+                                };
+
                                 page.Content()
-                                    .Column(column =>
-                                    {
-                                        column.Spacing(0.5f, Unit.Inch);
-
-                                        column.Item().Row(row =>
-                                        {
-                                            row.Spacing(0.25f, Unit.Inch);
-
-                                            row.ConstantItem(3.5f, Unit.Inch)
-                                               .Component(new PostComponent(postComponentProps[i]));
-
-                                            if (i + 1 < postComponentProps.Count)
-                                            {
-                                                row.ConstantItem(3.5f, Unit.Inch)
-                                                   .Component(new PostComponent(postComponentProps[i + 1]));
-                                            }
-                                        });
-
-                                        column.Item().Row(row =>
-                                        {
-                                            row.Spacing(0.25f, Unit.Inch);
-
-                                            if (i + 2 < postComponentProps.Count)
-                                            {
-                                                row.ConstantItem(3.5f, Unit.Inch)
-                                                   .Component(new PostComponent(postComponentProps[i + 2]));
-                                            }
-
-                                            if (i + 3 < postComponentProps.Count)
-                                            {
-                                                row.ConstantItem(3.5f, Unit.Inch)
-                                                   .Component(new PostComponent(postComponentProps[i + 3]));
-                                            }
-                                        });
-                                    });
+                                    .Component(pageContent);
 
                                 page.Footer().Dynamic(new PageFooterComponent(new PageFooterComponentProps { Date = $"{issue.Issue.DraftingEnd:MMMM yyyy}" }));
                             });
                         }
 
-                        int pages = (int)Math.Ceiling(postComponentProps.Count / 4.0) + 2;
+                        int pages = templatePostPairs.Count + 2;
                         int remainder = pages % 4;
 
                         if (remainder != 0)
@@ -345,7 +340,8 @@ namespace CherAmiAPI.BackgroundJobs
                     }).GeneratePdf(memoryStream);
 
                     Log.Error($"Uploading PDF for {recipient.Name}.");
-                    await UploadBlobAsync($"circles/{issue.CircleId}/issues/{issue.Issue.Id}/{issue.Issue.Title.Replace(" ", "_")}_{recipient.Name.Replace(" ", "_")}.pdf", memoryStream);
+                    //await UploadBlobAsync($"circles/{issue.CircleId}/issues/{issue.Issue.Id}/{issue.Issue.Title.Replace(" ", "_")}_{recipient.Name.Replace(" ", "_")}.pdf", memoryStream);
+                    await UploadBlobAsync($"circles/delete_me/{issue.Issue.Title.Replace(" ", "_")}_{recipient.Name.Replace(" ", "_")}.pdf", memoryStream);
                 }
 
                 //Log.Error($"Marking issue {issue.Issue.Id} as published.");
